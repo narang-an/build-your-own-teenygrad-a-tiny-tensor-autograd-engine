0e445a1307fed7b268e62cb51d7a8b8ab1e1f3ca
Anusha Narang <147870940+narang-an@users.noreply.github.com>
2026-08-13 19:44:45 -0400

    Step 27: sum_function_backward

--- model.py
+++ model.py
@@ -346,8 +346,10 @@ class Sum(Function):
         self.axis = axis
         return r(x, ReduceOps.SUM, axis)
 
-# Step 27 - sum_function_backward (not yet solved)
-# TODO: implement
+# Step 27 - sum_function_backward
+def backward(self, grad_output):
+    # broadcast the summed gradient back to the original input shape
+    return expand(grad_output, self.input_shape)
 
 # Step 28 - max_function_forward (not yet solved)
 # TODO: implement
